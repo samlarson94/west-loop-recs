43af74ab95481150f20920ac91f5567786813eae
Sam Larson <samlarson94@gmail.com>
2023-09-18 18:39:46 -0500

    Update models.py

--- brewery_map_project/brewery_map/models.py
+++ brewery_map_project/brewery_map/models.py
@@ -7,6 +7,8 @@ class Brewery(models.Model):
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     current_hours = models.CharField(max_length=100)
     full_hours = models.CharField(max_length=200)
-
+    s_rating = models.DecimalField(max_digits=3, decimal_places=0)
+    e_rating = models.DecimalField(max_digits=3, decimal_places=0)
+    
     def __str__(self):
         return self.name
