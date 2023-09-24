2a009b122229a42882a4769e8e1f04ddf8aff899
samlarson94 <samlarson94@gmail.com>
2023-09-24 13:03:03 -0500

    Adding bg and linking css.

--- brewery_map_project/brewery_map_project/settings.py
+++ brewery_map_project/brewery_map_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from . import config
-# import os
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,8 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Default primary key field type
